b03d2579991055b36b823c4b31e1ae193cd6bf30
jul1ana <julianaadt97@gmail.com>
2021-11-01 14:59:44 -0300

    primeiro commit

---
+++ src/module-info.java
@@ -0,0 +1,3 @@
+module primeiro_projeto {
+	
+}
---
+++ src/primeiro_projeto/OlaMundo.java
@@ -0,0 +1,10 @@
+package primeiro_projeto;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		System.out.print("Olá mundo");
+
+	}
+
+}
